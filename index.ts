96078d6e8d2b2af834149147762f5029e7ea6491
Joy <joykarmakar987654321@gmail.com>
2025-10-24 18:56:43 +0530

    secure false

--- index.ts
+++ index.ts
@@ -128,7 +128,7 @@ const User = mongoose.model('User', userSchema);
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
     port: Number(process.env.EMAIL_PORT) || 587,
-    secure: true, // Use false for port 587, true for port 465
+    secure: false, // Use false for port 587, true for port 465
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
